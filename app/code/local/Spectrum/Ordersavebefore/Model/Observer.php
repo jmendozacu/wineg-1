bdf9a5c5a3586acbcf4c3d476c4c442e5f7ca675
Navin Kumar <navin.kumar@spectrumted.com>
2016-06-16 18:38:05 +0530

    order attribute

--- app/code/local/Spectrum/Ordersavebefore/Model/Observer.php
+++ app/code/local/Spectrum/Ordersavebefore/Model/Observer.php
@@ -10,13 +10,15 @@ class Spectrum_Ordersavebefore_Model_Observer
 				$event = $observer->getEvent();
                 $order = $event->getOrder();
                 $location = Mage::getSingleton('core/session')->getAllstoresnewdatapicthree();
-                if ($location != "") 
+                $shipping =  Mage::getSingleton('checkout/session')->getQuote()->getShippingAddress()->getShippingMethod();
+
+                if ($location != "" && $shipping =="deliveryfromstore_deliveryfromstore") 
                 {
 
 	                $location = current($location);
 	                $output = $location["store_name"]." ".$location["address"]." ".$location["city"]." ".$location["zipcode"];
-	                $a = $shipping = $order->getShippingAddress()->getShippingMethod();
-	                Mage::log(print_r($a,true), null, 'mylogfile.log');
+	                
+	                //Mage::log(print_r($a,true), null, 'mylogfile.log');
 
 
 	                //$fieldVal = Mage::app()->getFrontController()->getRequest()->getParams();
